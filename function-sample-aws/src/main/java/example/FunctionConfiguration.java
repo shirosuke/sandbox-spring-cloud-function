df46245495fc83904daaf70fdaee3b69182ba415
shirosuke <tuketarou@gmail.com>
2021-05-01 23:39:36 +0900

    単体で動作するところまで確認。実際のCloudWatchEventsとの連携や、SAMのテンプレート化は未済。

--- function-sample-aws/src/main/java/example/FunctionConfiguration.java
+++ function-sample-aws/src/main/java/example/FunctionConfiguration.java
@@ -1,7 +1,12 @@
 package example;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
+import com.amazonaws.services.lambda.runtime.events.CloudWatchLogsEvent;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -25,5 +30,19 @@ public class FunctionConfiguration {
 	@Bean
 	public Function<String, Integer> wordcount() {
 		return String::length;
+	}
+
+	@Bean
+	public Function<ScheduledEvent, String> eventinfo() {
+		return e -> {
+			// TODO Java8のDatetimeに対応させる
+			ObjectMapper om = new ObjectMapper();
+			try {
+				return om.writerWithDefaultPrettyPrinter().writeValueAsString(e);
+			} catch (JsonProcessingException jsonProcessingException) {
+				jsonProcessingException.printStackTrace();
+			}
+			return null;
+		};
 	}
 }
